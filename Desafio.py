d0fa6aa27b4de6e83e6a32693ac67bc881186a10
IanPabl <ianpablofaria@gmail.com>
2025-08-08 16:17:26 -0300

    08/08/25

---
+++ Desafio.py
@@ -0,0 +1,8 @@
+nome = input('Digite seu nome:')
+idade = input('Digite sua idade:')
+cidade = input('Digite a sua cidade:')
+
+mensagem = f'Olá {nome},você tem {idade} anos e mora em {cidade}'
+print(mensagem)
+
+

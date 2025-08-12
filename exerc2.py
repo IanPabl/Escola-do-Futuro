43e947f0cd40bef6adcdc36c2984b8f319bbf0f9
IanPabl <ianpablofaria@gmail.com>
2025-08-12 16:36:46 -0300

    12/08/25

---
+++ exerc2.py
@@ -0,0 +1,3 @@
+n = int(input('Digite um número:'))
+
+print(f'O antecessor e o sucessor de {n} são {n-1} e {n+1}')
---
+++ exerc3.py
@@ -0,0 +1,8 @@
+n1 = float(input('Digite um número:'))
+n2 = float(input('Digite outro número:'))
+
+print(f'{n1} multiplicado por {n2} é igual a {n1*n2}')
+print(f'{n1} dividido por {n2} é igual a {n1/n2}')
+print(f'A divisão inteira de {n1} por {n2} é igual a {n1//n2}')
+print(f' O modolo de {n1} e {n2} é igual a {n1%n2}')
+print(f'{n1} elevado por {n2} é igual a {n1**n2}')
